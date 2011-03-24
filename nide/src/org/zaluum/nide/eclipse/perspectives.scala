511cf8975680548c5f30131d495f5ecc574d3b04
Frederic Perez Ordeig <frederic@zaluum.com>
2011-03-24 20:56:05 +0100

    deprecated ID_RES_NAV. classpath cleaned

--- nide/src/org/zaluum/nide/eclipse/perspectives.scala
+++ nide/src/org/zaluum/nide/eclipse/perspectives.scala
@@ -14,7 +14,7 @@ class BoxPerspective extends IPerspectiveFactory {
   def addViews() = {
     factory.createFolder("bottomLeft", IPageLayout.BOTTOM, 0.5f, factory.getEditorArea()).addView(IPageLayout.ID_PROBLEM_VIEW)
     factory.createFolder("bottomRight", IPageLayout.RIGHT, 0.5F, "bottomLeft").addView(IPageLayout.ID_PROP_SHEET)
-    factory.createFolder("topLeft", IPageLayout.LEFT, 0.3f, factory.getEditorArea()).addView(IPageLayout.ID_RES_NAV)
+    factory.createFolder("topLeft", IPageLayout.LEFT, 0.3f, factory.getEditorArea()).addView(IPageLayout.ID_PROJECT_EXPLORER)
     factory.createFolder("topLeftDown", IPageLayout.BOTTOM, 0.6f, "topLeft").addView(IPageLayout.ID_OUTLINE)
   }
   def addActionSets() = {
@@ -25,7 +25,7 @@ class BoxPerspective extends IPerspectiveFactory {
     factory.addNewWizardShortcut("org.zaluum.ide.editor.boxwizard");
   }
   def addViewShortcuts() = {
-    factory.addShowViewShortcut(IPageLayout.ID_RES_NAV);
+    factory.addShowViewShortcut(IPageLayout.ID_PROJECT_EXPLORER);
     factory.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
     factory.addShowViewShortcut(IPageLayout.ID_OUTLINE);
     factory.addShowViewShortcut(IPageLayout.ID_PROP_SHEET);
@@ -41,7 +41,7 @@ class DebugPerspective extends IPerspectiveFactory {
     addActionSets()
   }
   def addViews() = {
-    factory.createFolder("topLeft", IPageLayout.LEFT, 0.3f, factory.getEditorArea()).addView(IPageLayout.ID_RES_NAV)
+    factory.createFolder("topLeft", IPageLayout.LEFT, 0.3f, factory.getEditorArea()).addView(IPageLayout.ID_PROJECT_EXPLORER)
     factory.createFolder("topLeftDown", IPageLayout.BOTTOM, 0.6f, "topLeft").addView(IPageLayout.ID_OUTLINE)
   }
   def addActionSets() = {
