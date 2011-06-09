69da34f6dc610681b73e9b6c7b5feb98aeca5b2f
Frederic Perez Ordeig <frederic@zaluum.com>
2011-06-09 02:00:48 +0200

    multiplatform build

--- updatesite/buildProduct.py
+++ updatesite/buildProduct.py
@@ -11,6 +11,11 @@ features = [
   "org.zaluum.nide.feature.feature.group",
   "org.zaluum.jdt.patch.feature.group",
   "org.eclipse.draw2d.feature.group"]
+oses = [
+  ["linux","gtk","x86_64"],
+  ["linux","gtk","x86"],
+  ["win32","win32","x86"],
+  ["win32","win32","x86_64"]]
 def main():
   usage = "usage: %prog [options]"
   parser = optparse.OptionParser(usage)
@@ -18,19 +23,21 @@ def main():
 		  default='/home/frede/devel/eclipse3.6/eclipse',
 		  help="Point to eclipse binary")
   options, args = parser.parse_args()
-  osStr,wsStr,archStr = "linux","gtk","x86_64"
-  currentdir = os.getcwd()
-  destination = currentdir + "/target/products/zaluum.product/" + osStr + "/" + wsStr + "/" + archStr + "/zaluum"
-  # Merging branch requires latest merged master
-  def callDirector(iu,extraOpts):
-    c = options.eclipseCmd + """ -application org.eclipse.equinox.p2.director -nosplash -consolelog -repository http://127.0.0.1/3.6UpdatesMirror,http://download.eclipse.org/tools/gef/updates/releases/,http://127.0.0.1/heliosMirror,http://127.0.0.1/repo/,file:"""+currentdir+"""/target/repository/ -installIU """+ iu + """ -destination """+ destination + extraOpts 
-    output,_ = call_command(c)
-  logging.info('Materializing base product')
-  callDirector("zaluum.product", " -profile DefaultProfile -profileProperties org.eclipse.update.install.features=true -roaming -p2.os %s -p2.ws %s -p2.arch %s" % (osStr,wsStr,archStr))
-  for feature in features:
-    logging.info("Installing " + feature) 
-    callDirector(feature,"")
-  logging.info('Done') 
+  def exportProduct(osStr,wsStr,archStr):
+    currentdir = os.getcwd()
+    destination = currentdir + "/target/products/zaluum.product/" + osStr + "/" + wsStr + "/" + archStr + "/zaluum"
+    # Merging branch requires latest merged master
+    def callDirector(iu,extraOpts):
+      c = options.eclipseCmd + """ -application org.eclipse.equinox.p2.director -nosplash -consolelog -repository http://127.0.0.1/3.6UpdatesMirror,http://download.eclipse.org/tools/gef/updates/releases/,http://127.0.0.1/heliosMirror,http://127.0.0.1/repo/,file:"""+currentdir+"""/target/repository/ -installIU """+ iu + """ -destination """+ destination + extraOpts 
+      output,_ = call_command(c)
+    logging.info('Materializing base product for ' + osStr + " " + wsStr + " " + archStr)
+    callDirector("zaluum.product", " -profile DefaultProfile -profileProperties org.eclipse.update.install.features=true -roaming -p2.os %s -p2.ws %s -p2.arch %s" % (osStr,wsStr,archStr))
+    for feature in features:
+      logging.info("Installing " + feature) 
+      callDirector(feature,"")
+    logging.info('Done ' + osStr + " " + wsStr + " " + archStr) 
+  for o in oses:
+    exportProduct(o[0],o[1],o[2])
 def call_command(command):
   process = subprocess.Popen(command.split(' '),
 			      stdout=subprocess.PIPE,
