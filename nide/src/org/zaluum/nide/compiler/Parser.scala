8c933080d82337f14567fb6e4e5103576495adf5
Frederic Perez Ordeig <frederic@zaluum.com>
2011-06-23 02:31:07 +0200

    proguard initial configuration for ant task

--- nide/src/org/zaluum/nide/compiler/Parser.scala
+++ nide/src/org/zaluum/nide/compiler/Parser.scala
@@ -15,7 +15,8 @@ object Parser {
       parse(proto, Some(className))
     } catch {
       case e ⇒
-        // TODO fixme better handling 
+        // TODO fixme better handling
+        e.printStackTrace;
         println("PARSING ERROR!" +e )
         BoxDef(Name(""), Name(""), None, guiSize = Some(Dimension(250, 250)), None, List(), List(), List(), List(), List())
     }
