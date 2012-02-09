0186aafbc35d05d00412324e9d1a0599b3f876a5
Frederic Perez Ordeig <frederic@zaluum.com>
2012-02-09 16:10:08 +0100

    Merged V2.5.2 Groovy Eclipse plugin for Eclipse 3.7.1

--- build-mvn/fetch.sh
+++ build-mvn/fetch.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 DIR=`pwd`
-echo "dir $DIR"
-echo $0
 fetch()
 {
   COMMAND=$@
@@ -11,14 +9,4 @@ fetch()
 
 echo "scala tools"
 fetch -source http://download.scala-ide.org/scala-eclipse-toolchain-osgi-2.9.1.final -destination file:$DIR/scalaToolsMirror-2.9.1/
-
-echo "3.7 updates"
-fetch -source http://download.eclipse.org/eclipse/updates/3.7/ -destination file:$DIR/3.7UpdatesMirror/ 
-
-echo "indigo"
-fetch -source http://download.eclipse.org/releases/indigo/ -destination file:$DIR/indigoMirror/ 
-
-echo "gef"
-fetch -source http://download.eclipse.org/tools/gef/updates/releases/ -destination file:$DIR/gefMirror/ 
-
 
